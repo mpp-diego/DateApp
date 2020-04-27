2596d553b8305b6b5407c4916a360514e8c26aaa
Diego <diego.caridei@licentia.group>
2020-04-27 15:45:01 +0100

    Initial Commit

---
+++ DateApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DateApp
+//
+//  Created by MYPINPAD on 27/04/2020.
+//  Copyright © 2020 MYPINPAD. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
