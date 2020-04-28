6d820c3867ad11b90a967716101dfd58731a4c8a
Diego <diego.caridei@licentia.group>
2020-04-28 09:28:53 +0100

    first commit

--- DateApp/ContentView.swift
+++ DateApp/ContentView.swift
@@ -9,9 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    @State var contentFile = ""
+    private let fileName = "date_log.txt"
+    var body: some View
+    {
+        VStack
+        {
+            Button(action: write) { Text("Write") }.padding(10)
+            Button(action: readFile) { Text("Read") }.padding(10)
+            Text(self.contentFile).padding(10).padding(10)
+        }
     }
+    
+    func readFile() 
+    {
+        do
+        {
+            self.contentFile = try String(contentsOf: getDocumentsDirectory().appendingPathComponent(fileName), encoding: .utf8)
+        }
+        catch
+        {
+            print("Error read")
+        }
+    }
+    
+    func write()
+    {
+        let str = "\(Date())"
+        let filename = getDocumentsDirectory().appendingPathComponent(fileName)
+
+        do {
+            try str.write(to: filename, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            print("Error write")
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
